70c8137d4b8aa744a2903b2081183b17a0235602
Shivam Mehta <mehtashivam91@gmail.com>
2018-10-25 17:19:24 +0100

    ...

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import App from './components/App'
-import Projects from './components/Projects/ProjectsContainer'
-import ProjectReadme from './components/ProjectReadme/ProjectReadmeContainer'
+
 
 export default () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={App} />
-                <Route exact path='/:user' component={Projects} />
-                <Route exact path='/:user/:project' component={ProjectReadme} />
+
             </Switch>
         </BrowserRouter>
     )
