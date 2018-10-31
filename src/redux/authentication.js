7741dad36f887918b34e9fe112f21433c725bc46
Shivam Mehta <mehtashivam91@gmail.com>
2018-10-31 15:53:48 +0000

    POD's unique auth token

--- src/redux/authentication.js
+++ src/redux/authentication.js
@@ -12,32 +12,22 @@ const initialState = {
 }
 
 // Action constants
-// const IS_AUTHENTICATED = 'IS_AUTHENTICATED'
 const REDIRECT_TO_AUTH = 'REDIRECT_TO_AUTH'
 const AUTHENTICATE_USER = 'AUTHENTICATE_USER'
 
 // Action creators
-//export const isAuthenticated = () => ({ type: IS_AUTHENTICATED })
 export const redirectToAuth = () => ({ type: REDIRECT_TO_AUTH })
 export const authenticate = (search, history) => ({ type: AUTHENTICATE_USER, payload: { search, history } })
 
 // Helpers
 function checkforAuthToken() {
-    let auth_token = cookies.get('podToken-189247ca-76de-45c8-8d29-de3f4d8ff360')
+    let auth_token = cookies.get(`${process.env.REACT_APP_AUTH_TOKEN}`)
     return auth_token ? true : false
 }
 
 // Reducer
 export default (state = initialState, action) => {
     switch (action.type) {
-        // case IS_AUTHENTICATED:
-        //     console.log('Checking auth')
-        //     let auth_token = cookies.get('podToken-189247ca-76de-45c8-8d29-de3f4d8ff360')
-        //     return {
-        //         ...state,
-        //         authenticated: auth_token ? true : false,
-        //         token: auth_token
-        //     }
         case REDIRECT_TO_AUTH:
             console.log('Redirecting for auth')
             const url = `${process.env.REACT_APP_AUTH_URL}/login?client_id=${process.env.REACT_APP_CLIENT_ID}`
@@ -50,7 +40,7 @@ export default (state = initialState, action) => {
             const queryParams = new URLSearchParams(search)
             let token = queryParams.get('token')
             var expires = new Date(jwt_decode(token).exp * 1000)
-            cookies.set('podToken-189247ca-76de-45c8-8d29-de3f4d8ff360', token, { path: '/', expires: expires })
+            cookies.set(`${process.env.REACT_APP_AUTH_TOKEN}`, token, { path: '/', expires: expires })
             action.payload.history.push('/')
             return {
                 ...state,
