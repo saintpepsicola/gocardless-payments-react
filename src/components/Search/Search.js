df268f90c9a567c5270b161f023017db5ab17696
kevinlanzon <kevinlanzon@gmail.com>
2018-11-13 12:47:33 +0000

    Set focus on input when selecting the search tab and on pageload

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -9,15 +9,26 @@ import Button from '@material-ui/core/Button';
 import { Flex, Box } from 'reflexbox'
 
 export default class Search extends Component {
-    state = {
-      value: 0,
-    };
+  state = {
+    value: 0,
+    placeholder: 'SEARCH PATIENTS',
+    focus: true,
+  };
 
-    handleChange = (event, value) => {
-      this.setState({
-        value
-      });
-    };
+  handleChange = (event, value) => {
+    this.setState({
+      value,
+      placeholder: ''
+    });
+  };
+
+  setPlaceholder = (event) => {  
+    this.state.form.focus();
+    this.setState({
+      placeholder: 'SEARCH PATIENTS',
+      focus: true
+    });
+  };
 
   render() {
     const { value } = this.state;
@@ -27,15 +38,18 @@ export default class Search extends Component {
         <FlexContainer p={2} pb={0} pl={0} align='center'>
           <Box w={9/10}>
           <SearchBox
+            inputRef={e => { this.setState({form: e}) }}
             id='standard-full-width'
             fullWidth
+            placeholder={this.state.placeholder}
+            autoFocus={this.state.focus}
             margin='normal'
             InputProps={{
               startAdornment: (
                 <InputAdornment>
                 <Tabs value={value} onChange={this.handleChange} indicatorColor='primary'>
-                  <SearchTab label={<SearchGlass />} />
-                  <Tab label='ACTIVE (3)' />
+                  <SearchTab onClick={this.setPlaceholder} label={<SearchGlass />} />
+                  <Tab label='ACTIVE'  />
                   <Tab label='ARCHIVE' />
                 </Tabs>
                 </InputAdornment>
