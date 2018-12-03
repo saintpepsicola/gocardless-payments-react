25fa2276383e9759d16b5ed9028d8c69fd8adf5c
Jin Dai <jindai@ip-192-168-0-35.eu-west-2.compute.internal>
2018-12-03 11:11:18 +0000

    placed logout elsewhere

--- src/components/UserAvatar/UserAvatar.js
+++ src/components/UserAvatar/UserAvatar.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import avatarSrc from '../../resources/avatar.png'
+// import avatarSrc from '../../resources/avatar.png'
 import Typography from '@material-ui/core/Typography'
 import { Flex, Box } from 'reflexbox'
 
 export default class UserAvatar extends Component {
 
+    handleLogout() {
+        let confirm = window.confirm('Are you sure you wish to logout?')
+        if (confirm) {
+            this.props.logout()          
+        }
+    }
+
     render() {        
         return (
             <Avatar>
                 <Flex>
-                    <Box mr='15px'><Image alt='user-avatar' src={avatarSrc} /></Box>
+                    {/* <Box mr='15px'><Image alt='user-avatar' src={avatarSrc} /></Box> */}
                     <Box pt='6px'>
-                        <Name variant='subtitle1'>{this.props.userName}</Name>
-                        <Name variant='subtitle2'>{this.props.podName}</Name>
+                        <Name variant='subtitle1'>{this.props.userName} • {this.props.podName}</Name>
+                        <Name logout={true} variant='subtitle2' onClick={this.handleLogout.bind(this)}>Log out</Name>
                     </Box>
                 </Flex>
             </Avatar>
@@ -23,17 +30,20 @@ export default class UserAvatar extends Component {
 
 // Styled Components
 const Name = styled(Typography)`
-  color:white !important;
+&& {
+    color:white;
+    cursor: ${props => props.logout ? 'pointer' : 'auto'};
+}
 `
 
 const Avatar = styled.div`
   text-align:left;
 `
 
-const Image = styled.img`
-    text-align:left;
-    max-height:60px;
-    width:auto;
-    border-radius:50%;
-    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-`
+// const Image = styled.img`
+//     text-align:left;
+//     max-height:60px;
+//     width:auto;
+//     border-radius:50%;
+//     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+// `
