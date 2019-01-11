4be67558748bb3949bce2eb6ed4a5cb6a7a7ce00
shiv <new@ip-192-168-0-80.eu-west-2.compute.internal>
2019-01-11 10:28:10 +0000

    cleaner routes

--- src/components/Routes/ProtectedRoutes.js
+++ src/components/Routes/ProtectedRoutes.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 // Protected Routes
-import { Dashboard, OrderDetailsPage, OuickActionsPage } from './Views'
+import { Dashboard, OrderDetailsPage, SearchBar } from './Views'
 
 // These Routes are nested in App.js. The header stays static and we only change the content below it
 export const ProtectedRoutes = () => {
     return (
         <Switch>
-            <Route exact path={process.env.PUBLIC_URL + '/'} component={Dashboard} />
-            <Route exact path={process.env.PUBLIC_URL + '/qorder/:orderID'} component={OuickActionsPage} />
-            <Route exact path={process.env.PUBLIC_URL + '/order/:orderID'} component={OrderDetailsPage} />
+            <Route path='/' component={Layout} />
         </Switch>
     )
 }
+
+const Layout = () => (
+    <div>
+        <SearchBar />
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path='/order/:orderID' component={OrderDetailsPage} />
+    </div>
+)
+
--- src/components/Routes/Views.js
+++ src/components/Routes/Views.js
@@ -1,28 +1,11 @@
 import React from 'react'
 import RepeatsList from '../RepeatsList/RepeatsListContainer'
-import Search from '../Search/SearchContainer'
 import OrderDetails from '../OrderDetails/OrderDetailsContainer'
-import QuickActions from '../QuickActions/QuickActionsContainer'
+import Search from '../Search/SearchContainer'
 
 // DASHBOARD
-export const Dashboard = () => {
-        return (
-                <div>
-                        <Search />
-                        <RepeatsList />
-                </div>
-        )
-}
-
+export const Dashboard = () => <RepeatsList />
+// SEARCH
+export const SearchBar = () => <Search />
 // ORDER DETAILS
-export const OrderDetailsPage = () => {
-        return (
-                <div>
-                        <Search />
-                        <OrderDetails />
-                </div>
-        )
-}
-
-// QUICK ACTIONS
-export const OuickActionsPage = () => <QuickActions />
+export const OrderDetailsPage = () => <OrderDetails />
