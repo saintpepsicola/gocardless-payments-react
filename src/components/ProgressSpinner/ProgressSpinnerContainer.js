68a5229b01a9a87ad1e49621f04af08cd64ca7b0
shiv <new@ip-192-168-0-80.eu-west-2.compute.internal>
2019-02-07 19:17:04 +0000

    ...

--- src/components/ProgressSpinner/ProgressSpinnerContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import ProgressSpinner from './ProgressSpinner'
-
-const mapStateToProps = (state) => ({
-    fetching: state.repeats.fetching
-})
-
-export default connect(mapStateToProps, null)(ProgressSpinner)
