49639bf4cea9dbd52055bdd45b2a83abf0225209
shiv <new@ip-192-168-0-80.eu-west-2.compute.internal>
2019-02-25 10:47:09 +0000

    NULL NAME fix + animations from wassim 22/02

--- src/components/OrderDetails/OrderDetails.js
+++ src/components/OrderDetails/OrderDetails.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { withRouter } from "react-router"
 import QuickActions from '../QuickActions/QuickActionsContainer'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
@@ -34,90 +34,92 @@ class OrderDetails extends React.Component {
 
     render() {
         let dependent = this.props.repeat && this.props.repeat.dependent ? this.props.repeat.dependent : false
-        let { repeat, fetching } = this.props
+        let { repeat } = this.props
         let patient = repeat ? (repeat.dependent ? repeat.dependent : repeat.patient) : false
         return (
             <div>
-                {!fetching && repeat && <div>
-                    <Panel defaultExpanded={true}>
-                        <Summary expandIcon={<ExpandMoreIcon />}>
-                            <div>
-                                <PanelTitle>
-                                    {dependent && `${dependent.forename} ${dependent.surname}`}
-                                    {!dependent && `${repeat.patient_forename} ${repeat.patient_surname}`}
-                                </PanelTitle>
-                                {dependent && <PanelSubTitle>
-                                    {`Ordered by ${repeat.patient_forename} ${repeat.patient_surname}`}
-                                </PanelSubTitle>}
-                            </div>
-                        </Summary>
-                        <Content>
-                            <PatientDetails pl='24px' pr='24px' pt='8'>
-                                <Box w={3 / 12}>
-                                    <Title>PATIENT DETAILS</Title>
-                                    <Flex m='0'>
-                                        <Box w='90px'>
-                                            <p>NHS:</p>
-                                            <p>Gender:</p>
-                                            <p>Birthday:</p>
-                                            <p>E-mail:</p>
-                                            <p>Tel:</p>
-                                        </Box>
-                                        <Box w='249px'>
-                                            <p>{patient.nhs_number ? patient.nhs_number : '_'}</p>
-                                            <p>{patient.gender ? patient.gender : '_'}</p>
-                                            <p> {timestampToDate(patient.birthday)}</p>
-                                            <p>{repeat.patient && repeat.patient.username ? repeat.patient.username : '_'}</p>
-                                            <p>{patient.telephone ? patient.telephone : '_'}</p>
-                                        </Box>
-                                    </Flex>
-                                </Box>
-                                <Box w={3 / 12} >
-                                    <Title>ADDRESS</Title>
-                                    <Flex>
-                                        <Box>
-                                            <Address>
-                                                {!dependent && patient && patient.address && <span>{patient.address}<br /></span>}
-                                                {!dependent && patient && patient.postcode && <span>{patient.postcode}<br /></span>}
-                                                {dependent && dependent.address && dependent.address.address_line_1 && <span>{dependent.address.address_line_1}<br /></span>}
-                                                {dependent && dependent.address && dependent.address.address_line_2 && <span>{dependent.address.address_line_2}<br /></span>}
-                                                {dependent && dependent.address && dependent.address.city && <span>{dependent.address.city}<br /></span>}
-                                                {dependent && dependent.address && dependent.address.postcode && <span>{dependent.address.postcode}<br /></span>}
-                                            </Address>
-                                        </Box>
-                                    </Flex>
-                                </Box>
-                                <Box w={3 / 12} >
-                                    <Title>SURGERY</Title>
-                                    <Flex>
-                                        <Box>
-                                            <Address>
-                                                {repeat.surgery.address_1}<br />
-                                                {repeat.surgery.address_2}<br />
-                                                {repeat.surgery.address_3}<br />
-                                                {repeat.surgery.postcode}<br />
-                                            </Address>
-                                        </Box>
-                                    </Flex>
-                                </Box>
-                                <Box w={3 / 12} >
-                                    <Title>NOMINATED PHARMACY</Title>
-                                    <Flex>
-                                        <Box>
-                                            {repeat.pharmacy && <Address>
-                                                {repeat.pharmacy.pharmacy_name}<br />
-                                                {repeat.pharmacy.address}<br />
-                                                {repeat.pharmacy.postcode}<br />
-                                                {repeat.pharmacy.telephone}<br />
-                                            </Address>}
-                                        </Box>
-                                    </Flex>
-                                </Box>
-                            </PatientDetails>
-                        </Content>
-                    </Panel>
-                    <QuickActions />
-                </div>}
+                {repeat && <Container>
+                    <div>
+                        <Panel defaultExpanded={true}>
+                            <Summary expandIcon={<ExpandMoreIcon />}>
+                                <div>
+                                    <PanelTitle>
+                                        {dependent && `${dependent.forename} ${dependent.surname}`}
+                                        {!dependent && `${repeat.patient_forename} ${repeat.patient_surname}`}
+                                    </PanelTitle>
+                                    {dependent && <PanelSubTitle>
+                                        {`Ordered by ${repeat.patient_forename} ${repeat.patient_surname}`}
+                                    </PanelSubTitle>}
+                                </div>
+                            </Summary>
+                            <Content>
+                                <PatientDetails pl='24px' pr='24px' pt='8'>
+                                    <Box w={3 / 12}>
+                                        <Title>PATIENT DETAILS</Title>
+                                        <Flex m='0'>
+                                            <Box w='90px'>
+                                                <p>NHS:</p>
+                                                <p>Gender:</p>
+                                                <p>Birthday:</p>
+                                                <p>E-mail:</p>
+                                                <p>Tel:</p>
+                                            </Box>
+                                            <Box w='249px'>
+                                                <p>{patient.nhs_number ? patient.nhs_number : '_'}</p>
+                                                <p>{patient.gender ? patient.gender : '_'}</p>
+                                                <p> {timestampToDate(patient.birthday)}</p>
+                                                <p>{repeat.patient && repeat.patient.username ? repeat.patient.username : '_'}</p>
+                                                <p>{patient.telephone ? patient.telephone : '_'}</p>
+                                            </Box>
+                                        </Flex>
+                                    </Box>
+                                    <Box w={3 / 12} >
+                                        <Title>ADDRESS</Title>
+                                        <Flex>
+                                            <Box>
+                                                <Address>
+                                                    {!dependent && patient && patient.address && <span>{patient.address}<br /></span>}
+                                                    {!dependent && patient && patient.postcode && <span>{patient.postcode}<br /></span>}
+                                                    {dependent && dependent.address && dependent.address.address_line_1 && <span>{dependent.address.address_line_1}<br /></span>}
+                                                    {dependent && dependent.address && dependent.address.address_line_2 && <span>{dependent.address.address_line_2}<br /></span>}
+                                                    {dependent && dependent.address && dependent.address.city && <span>{dependent.address.city}<br /></span>}
+                                                    {dependent && dependent.address && dependent.address.postcode && <span>{dependent.address.postcode}<br /></span>}
+                                                </Address>
+                                            </Box>
+                                        </Flex>
+                                    </Box>
+                                    <Box w={3 / 12} >
+                                        <Title>SURGERY</Title>
+                                        <Flex>
+                                            <Box>
+                                                <Address>
+                                                    {repeat.surgery.address_1}<br />
+                                                    {repeat.surgery.address_2}<br />
+                                                    {repeat.surgery.address_3}<br />
+                                                    {repeat.surgery.postcode}<br />
+                                                </Address>
+                                            </Box>
+                                        </Flex>
+                                    </Box>
+                                    <Box w={3 / 12} >
+                                        <Title>NOMINATED PHARMACY</Title>
+                                        <Flex>
+                                            <Box>
+                                                {repeat.pharmacy && <Address>
+                                                    {repeat.pharmacy.pharmacy_name}<br />
+                                                    {repeat.pharmacy.address}<br />
+                                                    {repeat.pharmacy.postcode}<br />
+                                                    {repeat.pharmacy.telephone}<br />
+                                                </Address>}
+                                            </Box>
+                                        </Flex>
+                                    </Box>
+                                </PatientDetails>
+                            </Content>
+                        </Panel>
+                        <QuickActions />
+                    </div>
+                </Container>}
             </div>
         )
     }
@@ -211,10 +213,22 @@ padding: 0;
 }
 `
 
+const BounceAnimation = keyframes`
+0% {  opacity:0;transform:translateX(30px);}
+100% { opacity:1;transform:translateX(0);}
+`
+
 const Panel = styled(ExpansionPanel)`
 &&
 {
 background: none;
 box-shadow:none;
+}
+`
+
+const Container = styled.div`
+&&
+{
+animation: ${BounceAnimation} 0.4s ease-in-out forwards;
 }
 `
