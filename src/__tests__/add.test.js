cb3e31fdebdc85706cb2c25b83e815dcaa69c267
Oscar Sullivan <46930728+osulli@users.noreply.github.com>
2019-03-20 11:31:51 +0000

    make tests work

--- src/__tests__/add.test.js
+++ src/__tests__/add.test.js
@@ -1,10 +1,4 @@
-function add(a, b) {
-    return a + b;
-  }
-  
-  module.exports = add;
-
-const add = require('./add');
+const add = require('../add');
 describe('add', () => {
   it('should add two numbers', () => {
     expect(add(1, 2)).toBe(3);
